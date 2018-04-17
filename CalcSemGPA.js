4d4d27e580bf88c69975164f1fed4b1bae7a4114
Harika <harika1117@gmail.com>
2018-04-16 21:35:33 -0400

    updates

--- CalcSemGPA.js
+++ CalcSemGPA.js
@@ -1,5 +1,5 @@
 function calculate() {
   var trial = document.getElementById('g1').value;
   var trial2 = document.getElementById('ch1').value;
-  console.log(trial1 + " " + trail2);
+  console.log("PRINTS: " + trial1 + " " + trial2);
 }
