603617c2c549a599daa71f7ee3cd1a740490f94a
Harika <harika1117@gmail.com>
2018-04-16 23:14:01 -0400

    index points

--- semesterGPA.js
+++ semesterGPA.js
@@ -1,17 +1,69 @@
 function getValues()
 {
   var class1 = document.getElementById("class1").value;
-  var class2 = document.getElementById("gpa1").value;
   var class3 = document.getElementById("ch1").value;
-  document.write(class1 + " " + class2 + " " + class3);
+  var gpaVal1 = getNumbers();
+  var indexPoint = class3 * gpaVal;
+  window.alert("index points: " + indexPoint);
 }
 
 
 function getNumbers()
 {
+  var gpaVal;
   if (document.getElementById("grades").value == "A+")
   {
-    var four = 4.0;
-    window.alert(four);
+    gpaVal = 4.0;
+    //window.alert(gpaVal);
+  } else if (document.getElementById("grades").value == "A")
+  {
+    gpaVal = 4.0;
+    //window.alert(gpaVal);
+  } else if (document.getElementById("grades").value == "A-")
+  {
+    gpaVal = 3.7;
+    //window.alert(gpaVal);
+  } else if (document.getElementById("grades").value == "B+")
+  {
+    gpaVal = 3.3;
+    //window.alert(gpaVal);
+  } else if (document.getElementById("grades").value == "B")
+  {
+    gpaVal = 3.0;
+    //window.alert(gpaVal);
+  } else if (document.getElementById("grades").value == "B-")
+  {
+    gpaVal = 2.7;
+    //window.alert(gpaVal);
+  } else if (document.getElementById("grades").value == "C+")
+  {
+    gpaVal = 2.3;
+    //window.alert(gpaVal);
+  } else if (document.getElementById("grades").value == "C")
+  {
+    gpaVal = 2.0;
+    //window.alert(gpaVal);
+  } else if (document.getElementById("grades").value == "C-")
+  {
+    gpaVal = 1.7;
+    //window.alert(gpaVal);
+  } else if (document.getElementById("grades").value == "D+")
+  {
+    gpaVal = 1.3;
+    //window.alert(gpaVal);
+  } else if (document.getElementById("grades").value == "D")
+  {
+    gpaVal = 1.0;
+    //window.alert(gpaVal);
+  } else if (document.getElementById("grades").value == "D-")
+  {
+    gpaVal = 0.7;
+    //window.alert(gpaVal);
+  } else if (document.getElementById("grades").value == "F")
+  {
+    gpaVal = 0.0;
+    //window.alert(gpaVal);
   }
+  return gpaVal;
+
 }
